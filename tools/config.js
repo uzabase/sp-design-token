2c473a08834656129a6ff3717a0482309895fbfb
tsunaki.okubo <tsunaki.okubo@uzabase.com>
2023-02-10 14:59:09 +0900

    TypeScript変数の追加

---
+++ tools/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "source": ["tokens/**/*.json"],
+  "format": {
+    "typeScript/myFormat": ({dictionary}) => {
+      return "\n// Do not edit directly\n// Generated on " + new Date().toUTCString() + "\n\n" +
+          'export const colors = {\n' +
+          dictionary.allProperties.map(function(prop) {
+            // console.log(prop.path)
+            let to_ret_prop = '"' +prop.path.join('-') + '": "' + prop.value + '",';
+            if (prop.comment)
+              to_ret_prop = to_ret_prop.concat(' // ' + prop.comment);
+            return to_ret_prop;
+          }).join('\n') + "\n}";
+    }
+  },
+  "platforms": {
+    "css": {
+      "transformGroup": "css",
+      "buildPath": "../styles/",
+      "files": [
+        {
+          "destination": "speeda-tokens.css",
+          "format": "css/variables",
+          "options": {
+            "outputReferences": true
+          }
+        }
+      ]
+    },
+    "scss": {
+      "transformGroup": "scss",
+      "buildPath": "../styles/",
+      "files": [{
+        "destination": "speeda-tokens.scss",
+        "format": "scss/variables"
+      }]
+    },
+    "typeScript": {
+      "buildPath": "../styles/",
+      "files": [{
+        "destination": "speeda-tokens.ts",
+        "format": "typeScript/myFormat"
+      }]
+    }
+  }
+}
