1db3bed9b07dcacd580143504270d68b6e9c63a9
tsunaki.okubo <14939888+ski-0n0@users.noreply.github.com>
2024-11-21 21:18:28 +0900

    YakuHanJPs_Narrowもうなかった

--- lib/speeda-tokens.ts
+++ lib/speeda-tokens.ts
@@ -1,6 +1,6 @@
 
 // Do not edit directly
-// Generated on Wed, 20 Nov 2024 08:44:15 GMT
+// Generated on Thu, 21 Nov 2024 12:18:10 GMT
 
 export const spTokenTypes = [
 "font_family_ja",
@@ -9,6 +9,6 @@ export const spTokenTypes = [
 export type SpTokenTypes = (typeof spTokenTypes)[number];
 
 export const tokens: {[key in SpTokenTypes]:string} = {
-font_family_ja: "Arial, YakuHanJPs_Narrow, Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Noto Sans JP, sans-serif",
-font_family_zh: "Arial, YakuHanJPs_Narrow, 'PingFang SC', 'Microsoft YaHei', 'PingFang TC', Microsoft JhengHei, sans-serif",
+font_family_ja: "Arial, YakuHanJPs, Hiragino Sans, Hiragino Kaku Gothic ProN, Meiryo, Noto Sans JP, sans-serif",
+font_family_zh: "Arial, YakuHanJPs, 'PingFang SC', 'Microsoft YaHei', 'PingFang TC', Microsoft JhengHei, sans-serif",
 }
